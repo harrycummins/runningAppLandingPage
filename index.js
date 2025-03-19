3378dd3070e11981bd82a796bb31bd53f3162a20
harrycummins <115977764+harrycummins@users.noreply.github.com>
2025-03-19 14:03:57 +0000

    added routes aswell as google form

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public', 'index.html');  // /index.html'
   });
 
+  app.get('/googleForm', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'googleForm.html'));
+});
+
+app.get('/index.html', (req, res) => {
+  res.sendFile(__dirname + '/public', 'index.html');  // /index.html'
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
